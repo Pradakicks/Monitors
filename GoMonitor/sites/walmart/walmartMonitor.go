1e7d9e51c209ba9788dbf66906fe2eb7a2703b22
Pradakicks <70814894+Pradakicks@users.noreply.github.com>
2021-04-11 13:37:12 -0400

    Wamlart interface fix

--- GoMonitor/sites/walmart/walmartMonitor.go
+++ GoMonitor/sites/walmart/walmartMonitor.go
@@ -169,10 +169,10 @@ func (m *Monitor) monitor() error {
 		}
 
 	}
-	if realBody["payload"].(map[string]interface{})["images"].(map[string]interface{}) != nil {
+	if realBody["payload"].(map[string]interface{}) != nil {
 		image := realBody["payload"].(map[string]interface{})["images"].(map[string]interface{})
 		for _, value := range image {
-		if value.(map[string]interface{})["assetSizeUrls"].(map[string]interface{})["DEFAULT"] != nil {
+		if value.(map[string]interface{})["assetSizeUrls"].(map[string]interface{}) != nil {
 			m.Config.image = value.(map[string]interface{})["assetSizeUrls"].(map[string]interface{})["DEFAULT"].(string)
 		}
 		
