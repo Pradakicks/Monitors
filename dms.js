8ccd1cbbec14684052101b394ddf1b3155d1fa03
Adrian <adriancleve1@gmail.com>
2021-06-18 14:13:49 -0400

    Walmart on Dif servver

--- dms.js
+++ dms.js
@@ -610,9 +610,10 @@ async function startGoMonitor(currentBody, site){
 	try {
 		switch(site){
 			// case "2341":
-			case "TARGET":
-			case "GAMESTOP":
-			case "BESTBUY":
+			// case "TARGET":
+		// case "/"	case "GAMESTOP":
+		//	case "BESTBUY":
+			case "WALMART":
 				rp.post({
 					url : `${secondServer}:${port}/${site}`,
 					body : JSON.stringify(currentBody),
